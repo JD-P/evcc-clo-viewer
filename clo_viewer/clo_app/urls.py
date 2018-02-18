25a9fc3931fa968ea74c75dfc6edc00bf6e40775
John David Pressman <jdpostage@linuxmail.org>
2018-02-17 23:35:54 -0800

    Add CSS and skinning to degree program comparison

--- clo_viewer/clo_app/urls.py
+++ clo_viewer/clo_app/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^test/$', views.test_view),
+    url(r'^test/$', views.degree_program),
 ]
--- clo_viewer/clo_app/views.py
+++ clo_viewer/clo_app/views.py
@@ -6,23 +6,29 @@ from . import models
 
 # Create your views here.
 
-def test_view(request):
-    """Given a degree program, determine the distance between it and every other degree program."""
-    reference_degree_program = 80 # Hardcoded during testing, but will be from form
+def degree_program(request):
+    """Given a degree program, show the following information:
+
+    - Program name, number of credit hours, later a description if one is available
+    - What courses are involved in this degree and their core learning outcomes
+    - A comparison of the curriculum similarity this degree program has to every other degree program"""
+    ref_degree_program_id = 80 # Hardcoded during testing, but will be from form
+    rdp_object = models.DegreeProgram.objects.get(id=ref_degree_program_id)
     program_distances = []
     for degree_program in models.DegreeProgram.objects.exclude(
-            id=reference_degree_program):
+            id=ref_degree_program_id):
         union = models.Course.objects.none().union(
             models.DPCourseSpecific.objects.filter(
-                degree_program=reference_degree_program),
+                degree_program=ref_degree_program_id),
             models.DPCourseSpecific.objects.filter(
                 degree_program=degree_program.id)).count()
         overlap = models.Course.objects.filter(
-            dpcoursespecific__degree_program__id=reference_degree_program).filter(
+            dpcoursespecific__degree_program__id=ref_degree_program_id).filter(
                 dpcoursespecific__degree_program__id=degree_program.id).count()
         program_distances.append((degree_program, (overlap / union) * 100))
     program_distances.sort(key=lambda distance: distance[1])
     
     return render(request,
-                  'dp_compare.html',
-                  {"program_distances":program_distances})
+                  'degree_program.html',
+                  {"reference_program":rdp_object,
+                   "program_distances":program_distances})
